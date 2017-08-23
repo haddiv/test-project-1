277d0f367be54f1ee57811977e397f4bc9fcc7c2
asthik <astx.kras@gmail.com>
2017-08-23 22:35:51 +0500

    file

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*function getYear (startYear) {
+    var currentYear = new Date().getFullYear(), years = [];
+    startYear = startYear || 1980;
+
+    while ( startYear <= currentYear ) {
+        years.push(startYear++);
+    }
+
+    return years;
+}
+var years = getYear();
+console.log(years); */
