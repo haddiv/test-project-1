0e1bfcde1a28f7c6acf2f60f8f21d825d2f44cb7
Ashot1995 <ashotgharakeshishyan@gmail.com>
2017-08-24 23:53:54 +0400

    Angularjs Directives Controllers Filters

---
+++ lesson2/script.js
@@ -0,0 +1,10 @@
+angular.module('App', []).controller('nameCtrl', function($scope) {
+    $scope.names = [
+        {firstname:'Aram', lastname:'Manukyan'},
+        {firstname:'Sharl', lastname:'Aznavur'},
+        {firstname:'Viktor',lastname:'Hambardzumyan'}
+    ];
+});
+
+//////////
+
