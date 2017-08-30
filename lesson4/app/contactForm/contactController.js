adafa7ae1ed7b7bfa38afa2dd909d6c6bd903d3d
HGohar <goharhovhannisyan19@gmail.com>
2017-08-30 13:38:51 +0400

    Contact form was added

---
+++ lesson4/app/contactForm/contactController.js
@@ -0,0 +1,13 @@
+app.controller("IfMessageSubmited",function($scope){
+		
+		$scope.date = new Date();
+	    $scope.reset = function(){
+		$scope.name = "";
+		$scope.lastName = "";
+		$scope.select = "";
+		$scope.text = "";
+		$scope.email = "";
+		
+		
+		}	
+		})
