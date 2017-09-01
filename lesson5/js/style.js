8ca8e034b13d0875de931efa404d251d03b273a8
TigranMaghlodjyan <tigran.maghlodjyan@mail.ru>
2017-09-01 20:00:43 +0400

    lesson5

--- lesson5/js/style.js
+++ lesson5/js/style.js
@@ -1,16 +1,41 @@
 angular.module("appContinent",[]).controller("contrContinent",function($scope){
     $scope.continents = [
         {
+            "img":"europe.png",
             "continent":"Europe",
             "text":"Europe is a continent located entirely in the Northern Hemisphere and mostly in the Eastern Hemisphere. Europe is bordered by the Arctic Ocean to the north, the Atlantic Ocean to the west, and the Mediterranean Sea to the south. It comprises the westernmost part of Eurasia."
          },
          {
+             "img":"africa.png",
              "continent":"Africa",
              "text":"Africa is the world's second-largest and second-most-populous continent (the first being Asia). At about 30.3 million km2 (11.7 million square miles) including adjacent islands, it covers 6% of Earth's total surface area and 20.4% of its total land area."
           },
           {
+              "img":"australia.png",
              "continent":"Australia",
              "text":"Australia, sometimes known in technical contexts by the names Sahul, Australinea or Meganesia, to distinguish it from the Australian mainland, is a continent comprising mainland Australia, Tasmania, New Guinea, Seram, possibly Timor, and neighbouring islands."
            }
-    ]
+    ];
+    $scope.style = {
+        "width": '10%',
+        "heigth": '10%',
+        "margin-top": '5%',
+        
+    }
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
