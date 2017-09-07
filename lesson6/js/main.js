a1e9a3648647dc1dde9f882be93f0731bd7fe557
Ashot1995 <ashotgharakeshishyan@gmail.com>
2017-09-07 18:43:04 +0400

    add testemonials

--- lesson6/js/main.js
+++ lesson6/js/main.js
@@ -70,3 +70,41 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
     selector: "a[data-toggle=tooltip]"
   })
 });
+///////////////
+
+app.controller('MyCtrl', MyCtrl);
+
+function MyCtrl($scope) {
+    $scope.output = "0";
+    $scope.inOperation = false;
+    $scope.num1 = 0;
+
+    $scope.updateOutput = function(btn) {
+        if($scope.output == "0") {
+            $scope.output = btn;
+        } else {
+            $scope.output += String(btn);
+        }
+    };
+
+    $scope.operate = function(op) {
+        if($scope.output && !$scope.inOperation){
+            $scope.num1 = $scope.output;
+            $scope.output += op;
+            $scope.inOperation = true;
+        }else if($scope.output.length > $scope.num1.length+1){
+            $scope.output = eval($scope.output);
+            $scope.output += op;
+        }
+    };
+
+    $scope.equal = function() {
+        if($scope.output.length > $scope.num1.length+1){
+            $scope.output = eval($scope.output);
+            $scope.num1 = $scope.output;
+        }else{
+            $scope.output = $scope.num1;
+        }
+    };
+
+}
